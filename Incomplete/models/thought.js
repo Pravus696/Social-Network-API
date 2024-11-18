c673df2cef9c15077d64733dcf36494fe661791b
Pravus696 <tbell3393@gmail.com>
2024-11-18 17:02:55 -0600

    thought.js file in models completed

---
+++ Incomplete/models/thought.js
@@ -0,0 +1,40 @@
+import pkg from 'mongoose';
+import reactionSchema from './reaction.js';
+
+const { Schema, model } = pkg;
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal).toLocaleDateString(),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const thought = model('Thought', thoughtSchema);
+
+export default thought;
