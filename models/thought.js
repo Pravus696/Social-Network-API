c13fbb3a612e732090c8ac4deeb1f064cc21027c
Pravus696 <tbell3393@gmail.com>
2024-12-16 19:18:14 -0600

    added interfaces to schemas

--- models/thought.js
+++ models/thought.js
@@ -2,52 +2,80 @@ import { Document, model, Schema, ObjectId, Types } from "mongoose";
 
 // define the reaction interface
 interface IReaction extends Document {
-    reactionId: ObjectId;
-    reactionBody: string;
-    username: string;
-    createdAt: Date | string;
-  }
-  // define the thought interface
-  interface IThought extends Document {
-    thoughtText: string;
-    createdAt: Date | string;
-    username: string;
-    reactions: (typeof reactionSchema)[];
-    reactionCount: number;
-  }
+  reactionId: ObjectId;
+  reactionBody: string;
+  username: string;
+  createdAt: Date | string;
+}
+// define the thought interface
+interface IThought extends Document {
+  thoughtText: string;
+  createdAt: Date | string;
+  username: string;
+  reactions: (typeof reactionSchema)[];
+  reactionCount: number;
+}
 
-const thoughtSchema = new Schema(
+// create the reaction schema
+const reactionSchema = new Schema<IReaction>(
     {
-        thoughtText: {
-            type: String,
-            required: true,
-            minlength: 1,
-            maxlength: 280,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (createdAtVal) => val.toLocaleDateString(),
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        reactions: [reactionSchema],
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
     },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true,
-        },
-        id: false,
-    }
-);
+    reactionBody: {
+      type: String,
+      Required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (val: Date) => val.toLocaleString(),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  });
 
-thoughtSchema.virtual('reactionCount').get(function () {
-    return this.reactions.length;
+const thoughtSchema =new Schema<IThought>(
+    {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (val: Date) => val.toLocaleDateString(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  });
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
 });
 
-const Thought = model('Thought', thoughtSchema);
+const Thought = model("Thought", thoughtSchema);
 
 export default Thought;
--- models/user.js
+++ models/user.js
@@ -2,52 +2,51 @@ import { Document, Schema, model, ObjectId } from "mongoose";
 
 // define the user interface
 interface IUser extends Document {
-    username: string;
-    email: string;
-    thoughts: ObjectId[];
-    friends: ObjectId[];
-    friendCount: number;
-  }
+  username: string;
+  email: string;
+  thoughts: ObjectId[];
+  friends: ObjectId[];
+  friendCount: number;
+}
 
-const userSchema = new Schema(
+const userSchema = new Schema<IUser>(
     {
-        username: {
-            type: String,
-            unique: true,
-            required: true,
-            trim: true,
-        },
-        email: {
-            type: String,
-            unique: true,
-            required: true,
-            match: [/.+@.+\..+/, 'Must match an email address!'],
-        },
-        thoughts: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Thought',
-            },
-        ],
-        friends: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'User',
-            },
-        ],
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
     },
-    {
-        toJSON: {
-            virtuals: true,
-        },
-        id: false,
-    }
-);
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/.+@.+\..+/, "Must match an email address!"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  });
 
-userSchema.virtual('friendCount').get(function () {
-    return this.friends.length;
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 export default User;
